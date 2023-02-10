76127371a511ac80ae9d2c5cf1dcd23781590880
Colten Siemergit <colten50@hotmail.com>
2023-02-10 11:35:44 -0500

    updated src

--- src/data.js
+++ src/data.js
@@ -1,34 +1,41 @@
-export default [
+// import AnimationImg from './assets/svg/icon-animation.svg'
+// import BusinessImg from './assets/svg/icon-bussiness.svg'
+// import CryptoImg from './assets/svg/icon-crypto.svg'
+// import DesignImg from './assets/svg/icon-design.svg'
+// import PhotographyImg from './assets/svg/icon-Photography.svg'
+
+
+export const data = [
    
     { 
         id: 1,
-        img:"./src/assets/svg/icon-animation.svg",
+        img:'./assets/svg/icon-animation.svg',
         type:"Animation",
         para:"Learn the latest animation techniques to create stunning motion design and captivate your audience.", 
 
     },
     { 
         id: 2,
-        img:"./src/assets/svg/icon-design.svg",
+        img:'./assets/svg/icon-bussiness.svg',
         type:"Design",
         para:"Create beautiful, usable interfaces to help shape the future of how the web looks."
 
     },
     {
         id: 3,
-        img:"./src/assets/svg/icon-photography.svg",
+        img:'./assets/svg/icon-crypto.svg',
         type:"Photography",
         para: "Explore critical fundamentals like lighting, composition, and focus to capture exceptional photos."
     }, 
     { 
         id: 4,
-        img:"./src/assets/svg/icon-crypto.svg",
+        img:'./assets/svg/icon-design.svg', 
         type:"Photography",
         para: "All you need to know to get started investing in crypto. Go from beginner to advanced with this 54 hour course."
     },
     {
         id: 5,
-        img:"./src/assets/svg/icon-Business.svg",
+        img:'./assets/svg/icon-Photography.svg',
         type:"Photography",
         para: "A step-by-step playbook to help you start, scale, and sustain your business without outside investment."
 
